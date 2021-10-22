0ca256a57494c4a4f7022bb357d7667d3db178fa
boardguy1024 <boardguy1024@gmail.com>
2021-10-22 16:51:38 +0900

    Initial Commit

---
+++ Shared/SwiftUI_NewAppWithNewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NewAppWithNewsAPIApp.swift
+//  Shared
+//
+//  Created by park kyung seok on 2021/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NewAppWithNewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
